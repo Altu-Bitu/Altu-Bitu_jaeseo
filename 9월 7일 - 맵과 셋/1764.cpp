e7a425974d39e8586566e4b90aba6a694c6b1377
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-09-12 22:14:03 +0900

    [맵과 셋] 9월 12일

---
+++ 9월 7일 - 맵과 셋/1764.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	int n, m, cnt = 0;
+	string str;
+	cin >> n >> m;
+
+	vector<string> name;
+	vector<string> result;
+
+	//듣도 못한
+	while (n--) {
+		cin >> str;
+		name.push_back(str);
+	}
+
+	//사전 순으로 정렬
+	sort(name.begin(), name.end());
+
+	//보도 못한
+	while (m--) {
+		cin >> str;
+
+		//듣도 보도 못한
+		if (binary_search(name.begin(), name.end(), str)) { //Search
+			result.push_back(str);
+			cnt++;
+		}
+	}
+	//사전 순으로 정렬
+	sort(result.begin(), result.end());
+
+	cout << cnt << '\n';
+	for (int i = 0; i < result.size(); i++) cout << result[i] << '\n';
+
+}
---
+++ 9월 7일 - 맵과 셋/9375.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<map>
+using namespace std;
+
+//옷고르기
+int clothes(int n) {
+	string s, name;
+	map<string, int> c;
+
+	while (n--) {
+		cin >> s >> name;
+		c[name]++;
+	}
+
+	//의상 종류에 그 의상 안 입은 경우 다 더해서 곱함 -1
+	int cnt = 1;
+	for (auto iter : c)
+		cnt *= (iter.second + 1);
+
+	return cnt - 1;
+}
+
+int main() {
+	int t, n;
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> n;
+		cout << clothes(n) << '\n';
+	}
+}
