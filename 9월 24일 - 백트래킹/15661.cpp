47a241b3285107df1ec67bb7d7271635c1ef6dde
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-09-30 01:51:44 +0900

    [백트래킹] 9월 30일

---
+++ 9월 24일 - 백트래킹/15661.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+
+const int SIZE = 21;
+int n;
+int team[SIZE][SIZE];
+bool check[SIZE]; //백트래킹으로 나눈 팀은 true(start팀), 나머지 팀은 false(link팀)
+int result = 2147483647; //최솟값을 구하기 위한 int에서 최댓값
+
+void statMin(int cnt) {
+	if (cnt == n) {
+		int start = 0, link = 0;
+
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++) {
+				if (check[i] && check[j]) start += team[i][j];
+				if (!check[i] && !check[j])link += team[i][j];
+			}
+
+		int tmp = abs(start - link);
+		if (tmp < result) result = tmp;//최솟값..
+	}
+
+	for (int i = cnt; i <= n; i++)
+		if (!check[i]) {
+			check[i] = true;
+
+			statMin(i);
+			check[i] = false;
+		}
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> team[i][j];
+
+	statMin(1);
+	cout << result;
+}
---
+++ 9월 24일 - 백트래킹/1759.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool is_vowel(char c) {
+	switch (c)
+	case 'a': case 'e': case 'i': case'o': case'u': return true;
+	return false;
+}
+int main() {
+	int l, c;
+	cin >> l >> c;
+	vector<char> alph(c, 0);
+	vector<bool> check(c, false);
+
+	for (int i = 0; i < c; i++)
+		cin >> alph[i];// 알파벳 입력
+	sort(alph.begin(), alph.end()); // 알파벳 오름차순 정렬
+
+	for (int i = 0; i < l; i++)
+		check[i] = true;// l개 알파벳 true로 셋팅
+
+	do {
+		string s = ""; //암호
+		int vo = 0, co = 0;//모음과 자음 개수
+
+		for (int i = 0; i < c; i++)
+			if (check[i]) {
+				if (is_vowel(alph[i]))
+					vo++;
+				else
+					co++;
+				s += alph[i];
+			}
+		if (vo >= 1 && co >= 2)	//단어가 되는 조건 모음이 적어도 하나, 자음이 적어도 두 개
+			cout << s << '\n';
+	} while (prev_permutation(check.begin(), check.end()));
+}
