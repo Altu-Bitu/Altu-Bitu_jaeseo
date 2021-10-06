7dc5c75c75965267e13227c4b8ef29d2ea1d3e5a
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-10-07 01:39:51 +0900

    [그리디 알고리즘] 10월 7일 - Update
    수정했습니다..!!! 올려주시는 샘플코드 보고 참고해보도록 하겠습니다... 흫ㅎ 항상 감사합니다!!

--- 10월 1일 - 그리디 알고리즘/16206.cpp
+++ 10월 1일 - 그리디 알고리즘/16206.cpp
@@ -6,46 +6,57 @@ using namespace std;
 int cutting(int& m, int cake) {
 	int sum = 0;//각 케이크마다 길이가 10 인 케이크 개수
 
-	while (cake >= 10) {// 케이크 길이가 여전히 10 이상일 때
-		if (cake == 10) {
-			sum++;
-			break;// 길이가 10인 케이크는 더 이상 자를 필요 없으니 break
+	if (cake < 10) return 0;//케이크 길이가 10 미만인 건 못 먹음
+
+	//10 이상인 케이크 중에서
+	//길이가 10인 케이크
+	if (cake == 10) return 1;
+
+	// 길이가 10 초과인 케이크
+	// 자를 기회가 남았다면
+	if (m > 0) {
+		int tmp = m - (cake / 10);
+		if (cake % 10 == 0) tmp++;//10 배수는 1번 덜 잘라도 됨
+
+		//자르려는 케이크 길이가 횟수보다 길면
+		//남은 횟수만큼만 자르기
+		if (tmp < 0) {
+			sum = m;
+			m = 0;
 		}
-		else {//자를 횟수 있으면 자름
-			if (m <= 0) break;
-			m--;
-			sum++;
-			cake -= 10;
+		//케이크 잘라서 길이 10 만들기
+		else {
+			sum += cake / 10;
+			m -= cake / 10;
+			if (cake % 10 == 0) m++;//10 배수는 1번 덜 잘라도 됨
 		}
 	}
 	return sum;
 }
 
+bool cmp(const int& c1, const int& c2) {
+	if (c1 % 10 == 0 && c2 % 10 == 0) return c1 < c2; //10의 배수 중에도 같아도 작은 숫자
+
+	// 1. 둘 중 c1이 10 의 배수 : swap안됨
+	// 2. 둘 중 c2이 10 의 배수 : swap됨
+	// 3. 둘 다 10의 배수가 아님 : swap됨(10의 배수 아닌 수는 swap되든 상관 없음)
+	return (c1 % 10 == 0);
+}
+
 int main() {
-	int n, m, tmp;//롤케이크 개수n, 자를 수 있는 최대 횟수m
+	int n, m;//롤케이크 개수n, 자를 수 있는 최대 횟수m
 	cin >> n >> m;
 
 	int sum = 0;//길이가 10 인 케이크 개수
 
-	vector<int> cakeTen, cake;//10의 배수인 케이크, 나머지 케이크
+	vector<int> cake(n, 0);
 
-	for (int i = 0; i < n; i++) {
-		cin >> tmp;
+	for (int i = 0; i < n; i++)
+		cin >> cake[i];
 
-		if (tmp % 10 == 0)
-			cakeTen.push_back(tmp);
-		else
-			cake.push_back(tmp);
-	}
+	sort(cake.begin(), cake.end(), cmp);//10의 배수이며, 작은 숫자 정렬로
 
-	sort(cake.begin(), cake.end());
-	sort(cakeTen.begin(), cakeTen.end());
-
-	//10의 배수 케이크가 자르는 횟수가 1 적으니까 먼저 처리
-	for (int i = 0; i < cakeTen.size(); i++)
-		sum += cutting(m, cakeTen[i]);
-
-	for (int i = 0; i < cake.size(); i++)
+	for (int i = 0; i < n; i++)
 		sum += cutting(m, cake[i]);
 
 	cout << sum;
