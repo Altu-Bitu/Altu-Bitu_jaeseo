d4cb3a2185008a81eb371913c1b3b318e2c57d73
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-11-01 23:52:34 +0900

    [DFS와 BFS] 11월 1일
    처음에 DFS와 BFS의 활용법?이 확 와닿지 않아서 헤매버렸습니다...@@ 그래서 특히 4963번은 구글링 해가며 열심히 공부하였습니다..! 그리고 19538번...은 더 고민해보며,, 샘플코드를 올려주시면 주석으로,,, 열심히 공부하겠습니다... 정말정말 항상 감사합니다!!

---
+++ 10월 15일 - DFS와 BFS/19538.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+const int MAX=200000+1;
+
+int rumor[MAX];//루머 믿기 시작한 시간
+vector<int> spread;//처음 유포자
+vector<int> persons[MAX];//주변인들 관계
+
+void dfs() {
+	queue<int> q;
+
+}
+
+int main() {
+	int n, m, p;//n: 사람 수, m: 최초유포자 번호, p: 주변인들
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)//주변인들 관계 입력
+		while (true) {
+			cin >> p;
+			if (p == 0) break;
+			persons[i].push_back(p);//i번째 줄의 i번 주변사람들
+		}
+
+	cin >> m;
+	spread.assign(m,0);
+	for (int i = 0; i < m; i++) {
+		cin >> spread[i];
+		rumor[spread[i]] = 0;//유포자는 0분부터 믿기 시작
+	}
+
+	dfs();
+
+}
