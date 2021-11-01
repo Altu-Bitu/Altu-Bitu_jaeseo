b90ac3583907b3146d87990f0a8d5025c1a20780
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-11-01 23:32:04 +0900

    [투 포인터] 11월 1일
    모두 ,, 열심히 임하려고 노력했습니다... 테트로미노는... 샘플코드에 주석달며 더 열심히 공부하겠습니다..........항상 감사합니다!

---
+++ 10월 12일 - 투 포인터/14500.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int findMax(vector<vector<int>> squ) {
+	int sum = 0;
+
+
+	return sum;
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> squ(n, vector<int>(m, 0));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> squ[i][j];
+
+	cout << findMax(squ);
+}
---
+++ 10월 12일 - 투 포인터/2607.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+
+bool comp(int a[], int b[]) {
+	int sum = 0, check = 0;
+	for (int i = 0; i < 26; i++) {
+		sum += abs(a[i] - b[i]);//문자 구성 차이
+		check += a[i] - b[i];//글자 길이 차이
+	}
+
+	//한 단어에서 하나 더하거나 빼거나 다른 문자로 바꿈-> 문자 구성 개수 차이가 1개 이하
+	//같은 길이, 같은 구성
+	if (sum < 2 || (sum == 2 && check == 0)) return true;
+	return false;
+}
+
+int main() {
+	int n, ans = 0;
+	string s;
+
+	cin >> n;
+	int alph1[26] = { 0 };
+	cin >> s;//첫 번째 단어
+	for (int i = 0; i < s.size(); i++)
+		alph1[s[i] - 'A']++;
+
+	while (--n) {
+		int alph2[26] = { 0 };
+		cin >> s;//비교할 단어
+		for (int i = 0; i < s.size(); i++)
+			alph2[s[i] - 'A']++;
+		if (comp(alph1, alph2)) ans++;
+	}
+
+	cout << ans;
+}
