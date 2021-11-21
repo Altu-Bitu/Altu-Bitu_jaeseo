47e27591e78eebf54a0c703d923057b34688daa3
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2021-11-21 13:20:44 +0900

    [트리] 11월 21일
    감사합니다!!!! 필수문제는,, 샘플코드 보며,,, 공부,,하겠습니다,,,!

---
+++ 11월 16일 - 트리/15681.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> tree;
+vector<int> treeNum;
+vector<bool> visited;
+
+//dfs
+int dfs(int root) {
+	if (treeNum[root])//해당 정점에, subtree있을 시
+		return treeNum[root];//연결된 subtree의 정점 개수 리턴
+
+	//처음방문
+	visited[root] = true;
+	treeNum[root] = 1;//자신도 자신을 루트로 하는 서브트리에 포함되므로 1부터 시작
+	for (int i = 0; i < tree[root].size(); i++) {
+		int child = tree[root][i];//자식노드
+		if (!visited[child])//방문안했을 경우
+			treeNum[root] += dfs(child);
+	}
+
+	return treeNum[root];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, r, q;//n: 정점수, r: 루트번호, q:쿼리수
+	int u, v, input;
+
+	cin >> n >> r >> q;
+
+	//할당
+	tree.assign(n + 1, vector<int>());
+	visited.assign(n + 1, false);
+	treeNum.assign(n + 1, 0);
+
+	//tree생성
+	n--;
+	while (n--) {
+		cin >> u >> v;
+		tree[u].push_back(v);
+		tree[v].push_back(u);
+	}
+
+	//dfs
+	treeNum[r] = dfs(r);//찾을 루트번호
+
+	while (q--) {
+		cin >> input;
+		cout << treeNum[input] << '\n';
+	}
+}
---
+++ 11월 16일 - 트리/1967.cpp
@@ -0,0 +1,52 @@
+﻿//풀이참고 : https://codingwell.tistory.com/61
+#include<iostream>
+#include<vector>
+using namespace std;
+
+const int SIZE = 10000 + 1;
+vector<pair<int, int>> tree[SIZE];//first: 자식, second: 간선 가중치(길이)
+vector<bool> visited;
+int dia = 0, far = 0;//지름, 가장 멀리있는 점
+
+void dfs(int n, int len) {
+	if (visited[n])//방문했으면 리턴
+		return;
+
+	//처음방문
+	visited[n] = true;
+
+	if (dia < len) far = n;	//가장 멀리있는 점 갱신
+	dia = max(dia, len);	//지름 갱신
+
+	for (int i = 0; i < tree[n].size(); i++) {
+		int child = tree[n][i].first;//자식
+		int weight = len + tree[n][i].second;//길이 = 현재 길이 + 자식 가중치
+
+		dfs(child, weight);
+	}
+}
+
+int main() {
+	int n, a, b, c;
+
+	cin >> n;
+	visited.assign(n + 1, false);
+
+	for (int i = 0; i < n - 1; i++) {
+		cin >> a >> b >> c;
+		tree[a].push_back({ b,c });
+		tree[b].push_back({ a,c });
+	}
+
+	//가장 끝 점에 있는 두 지점이 지름이기 때문에
+	//첫 노드에서 가장 멀리 있는 점과 그 점에서 가장 멀리 있는 점의 거리를 구하기
+
+	dfs(1, 0);//가장 멀리있는 점 구하기(첫 노드는 항상 1, 처음 지름은 0)
+
+	dia = 0;
+	visited.assign(n + 1, false);
+
+	dfs(far, 0);//지름 구하기(제일 멀리있는 점, 처음 지름은 0)
+
+	cout << dia << '\n';
+}
